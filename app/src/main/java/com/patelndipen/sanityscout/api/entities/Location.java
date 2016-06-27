e7bf615407c284d2dd47eb311af24cd77b5a2d8b
mangoslicer <patelndipen@gmail.com>
2016-06-27 12:12:39 -0400

    Updated Location entity to correspond to backend rest api's Location entity

--- app/src/main/java/com/patelndipen/sanityscout/api/entities/Location.java
+++ app/src/main/java/com/patelndipen/sanityscout/api/entities/Location.java
@@ -2,67 +2,54 @@ package com.patelndipen.sanityscout.api.entities;
 
 import android.support.annotation.NonNull;
 
-import com.google.auto.value.AutoValue;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.google.auto.value.AutoValue;
 
-import java.sql.Time;
-
-/**
- * The Location class contains attributes common to all location (e.g. study, dining, and rec locations)
- */
 @AutoValue
 @JsonDeserialize(builder = AutoValue_Location.Builder.class)
 public abstract class Location {
 
-    private static final String JSON_PROPERTY_ID = "id";
-    private static final String JSON_PROPERTY_LOCATIONNAME = "location_name";
-    private static final String JSON_PROPERTY_OPENINGTIME = "opening_time";
-    private static final String JSON_PROPERTY_CLOSINGTIME = "closing_time";
-    private static final String JSON_PROPERTY_ADDRESS = "address";
-
     @NonNull
-    @JsonProperty(JSON_PROPERTY_ID)
-    public abstract String id();
-
-    @NonNull
-    @JsonProperty(JSON_PROPERTY_LOCATIONNAME)
     public abstract String locationName();
 
     @NonNull
-    @JsonProperty(JSON_PROPERTY_OPENINGTIME)
-    public abstract Time openingTime();
+    public abstract String locationType();
 
     @NonNull
-    @JsonProperty(JSON_PROPERTY_CLOSINGTIME)
-    public abstract Time closingTime();
-
-    @NonNull
-    @JsonProperty(JSON_PROPERTY_ADDRESS)
     public abstract String address();
+
+    @NonNull
+    public abstract String openingTime();
+
+    @NonNull
+    public abstract String closingTime();
+
+    @NonNull
+    public abstract String[] locationLuxuries();
+
+    @NonNull
+    public abstract String imagePath();
 
     @AutoValue.Builder
     public static abstract class Builder {
 
         @NonNull
-        @JsonProperty(JSON_PROPERTY_ID)
-        public abstract Builder id(@NonNull String id);
+        public abstract Builder locationName(@NonNull final String locationName);
 
         @NonNull
-        @JsonProperty(JSON_PROPERTY_LOCATIONNAME)
-        public abstract Builder locationName(@NonNull String locationName);
+        public abstract Builder address(@NonNull final String address);
 
         @NonNull
-        @JsonProperty(JSON_PROPERTY_OPENINGTIME)
-        public abstract Builder openingTime(@NonNull Time openingTime);
+        public abstract Builder openingTime(@NonNull final String openingTime);
 
         @NonNull
-        @JsonProperty(JSON_PROPERTY_CLOSINGTIME)
-        public abstract Builder closingTime(@NonNull Time closingTime);
+        public abstract Builder closingTime(@NonNull final String closingTime);
 
         @NonNull
-        @JsonProperty(JSON_PROPERTY_ADDRESS)
-        public abstract Builder address(@NonNull String address);
+        public abstract Builder locationLuxuries(@NonNull final String[] locationLuxuries);
+
+        @NonNull
+        public abstract Builder imagePath(@NonNull final String imagePath);
 
         @NonNull
         public abstract Location build();
